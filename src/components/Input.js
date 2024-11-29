84022b127b85c69f0f35bbbde770658714265e53
Darshandm07 <drshndm@gmail.com>
2024-11-29 16:17:10 +0530

    first commit

---
+++ src/components/Input.js
@@ -0,0 +1,15 @@
+import "./Input.css";
+const Input = (props) => {
+  return (
+    <div className="input-wrapper">
+      <div className="result">
+        <h1>{props.result}</h1>
+      </div>
+      <div className="text">
+        <h3>{props.text}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Input;
